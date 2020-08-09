a97e50291260eb3eec0eed9d1b1d3ea3b5d28072
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 02:07:45 -0300

    feat: add new action for Register User

--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -4,6 +4,10 @@ export const ActionLoginUser = ({ commit }, payload) => {
   commit(types.LOGIN_USER, payload);
 };
 
+export const ActionRegisterUser = ({ commit }, payload) => {
+  commit(types.REGISTER_USER, payload);
+};
+
 export const ActionLogoutUser = ({ commit }, payload) => {
   commit(types.LOGOUT_USER, payload);
 };
