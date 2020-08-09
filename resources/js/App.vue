e9ab7ecea3ee76833ac7d1c5726d4e4d20bb1e3a
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:29:25 -0300

    feat: add vue root component

---
+++ resources/js/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-100">
+    <navbar :app="this" />
+
+    <router-view :app="this" />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "app",
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      user: null,
+      req: axios.create({
+        baseUrl: "{{  URL::to(" / ")  }}"
+      })
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.req.get("auth/init").then(({ data }) => {
+        this.$store.dispatch("ActionLoginUser", data.user);
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
