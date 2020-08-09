a0b38d5ced61c9188c7abc399d050813754d00b1
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:53:10 -0300

    feat: add login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+
+use App\User;
+
+class LoginController extends Controller
+{
+  public function init() {
+    try {
+      $user = Auth::user();
+      return response()->json(['user' => $user], 200);
+    }catch (Exception $e){
+      var_dump($e);
+      return response('Login internal error', 500);
+    }
+  }
+
+  public function login(Request $request) {
+    try {
+      $credentials = $request->only('email', 'password');
+
+      $user = User::where('email', $request->email)->first();
+
+      $messages = [
+          'required' => 'The :attribute field is required.',
+          'exists' => 'The :attribute not exist.',
+        ];
+
+      $validator = Validator::make($request->all(), [
+          'email' => 'required|exists:users',
+          'password' => 'required',
+      ], $messages);
+
+
+      if ($validator->fails()) {
+        return response($validator->errors()->first(), 400);
+      }
+
+      if (!Hash::check($request->password, $user->password)) {
+        return response('Incorrect Password', 400);
+      }
+
+      Auth::attempt($credentials, true);
+
+      $user = Auth::user();
+
+      return response()->json(['user' => $user], 200);
+
+    }catch (Exception $e) {
+        var_dump($e);
+        return response('Login internal error', 500);
+    }
+  }
+
+  public function logout() {
+    try {
+      Auth::logout();
+    }catch (Exception $e) {
+      var_dump($e);
+      return response('Login internal error', 500);
+    }
+  }
+}
