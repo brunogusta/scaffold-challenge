1393b2652dc66b4fb47a96829141d08dd637015b
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:12:55 -0300

    feat: add action to vuex

---
+++ resources/js/store/actions.js
@@ -0,0 +1,9 @@
+import * as types from "./mutation-types";
+
+export const ActionLoginUser = ({ commit }, payload) => {
+  commit(types.LOGIN_USER, payload);
+};
+
+export const ActionLogoutUser = ({ commit }, payload) => {
+  commit(types.LOGOUT_USER, payload);
+};
