e97817bd77a673988e0ad219dc70e6d77cd03797
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:35:57 -0300

    feat: add js configuration file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+
+import VueRouter from "vue-router";
+import Embed from "v-video-embed";
+import VModal from "vue-js-modal";
+
+import routes from "./routes";
+import App from "./App.vue";
+import store from "./store";
+
+Vue.use(VueRouter);
+Vue.use(Embed);
+Vue.use(VModal);
+
+const router = new VueRouter({
+  routes
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
