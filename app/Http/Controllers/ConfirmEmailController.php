a0afa0120ee44ff99b0ef6f60a2a3d27ee274086
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 16:51:01 -0300

    feat: add new controller for confirm email  requests

--- app/Http/Controllers/ConfirmEmailController.php
+++ app/Http/Controllers/ConfirmEmailController.php
@@ -5,10 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
+use App\User;
+
 class ConfirmEmailController extends Controller
 {
-    public function confirm() {
-        $token = Input::get('token');
-        info($token);
+  public function confirm(Request $request, $hash) {
+    try{
+      User::where('email_confirm_token', $hash)
+        ->update([
+          'confirmed' => true,
+          'email_confirm_token' => null
+          ]);
+
+      return response()->json(['message' => 'E-mail confirmed.']);
+    }catch(Exception $e) {
+      return response($e, 500);
     }
+  }
 }
