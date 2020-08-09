80cb023d277a22cec403640b12201c1b808eddbe
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:49:52 -0300

    feat: add exceptions to test requests

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'http://127.0.0.1:8000/auth/*',
+        'http://127.0.0.1:8000/videos/*'
     ];
 }
