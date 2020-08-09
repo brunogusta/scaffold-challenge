16f5cb112c54c716b0654f71c2bfa5280b75e0ef
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 00:50:59 -0300

    feat: add new routes in web router

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('home');
 });
+
+
+
+Route::prefix('auth')->group(function () {
+  Route::get('init', 'LoginController@init');
+
+  Route::post('login', 'LoginController@login');
+  Route::post('logout', 'LoginController@logout');
+  Route::post('register', 'RegisterController@register');
+  Route::post('forgot-password', 'ForgotPasswordController@forgot');
+  Route::post('reset-password', 'ResetPasswordController@reset');
+  Route::put('confirm-email/{token}', ['uses' => 'ConfirmEmailController@confirm']);
+});
+
+Route::middleware(['auth', 'teacher'])->group(function () {
+  Route::get('/videos', 'VideoController@userVideos');
+  Route::post('/videos', 'VideoController@store');
+  Route::put('/videos/{id}', 'VideoController@edit');
+  Route::delete('/videos/{id}', 'VideoController@destroy');
+});
+
+Route::get('/videos/watch', 'VideoController@index')->middleware(['auth']);
