e8323c78400063f85f46ad3818bc70107c832cae
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:42:30 -0300

    feat: add new table migration for videos

---
+++ database/migrations/2020_08_08_225413_create_videos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name')->nullable(false);
+            $table->string('url')->nullable(false);
+            $table->string('thumbUrl')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
