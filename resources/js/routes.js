a1b0db9756adc710bee5a61019d7b8749cf8104b
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 16:44:13 -0300

    feat: add new routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,13 @@
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
+import Videos from "./pages/Videos";
+import Users from "./pages/Users";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResendEmailConfirmation from "./pages/auth/ResendEmailConfirmation";
+import ResetPassword from "./pages/auth/ResetPassword";
+import ConfirmEmail from "./pages/auth/ConfirmEmail";
+
 import VideosManagement from "./pages/VideosManagement";
 
 import store from "./store";
@@ -17,9 +24,53 @@ export default [
     name: "register"
   },
   {
+    path: "/forgot-password",
+    component: ForgotPassword,
+    name: "forgot-password"
+  },
+  {
+    path: "/resend-email-confirmation",
+    component: ResendEmailConfirmation,
+    name: "resend-email-confirmation"
+  },
+  {
+    path: "/confirm-email/:hash",
+    component: ConfirmEmail,
+    name: "confirm-email"
+  },
+  {
+    path: "/reset-password/:hash",
+    component: ResetPassword,
+    name: "reset-password"
+  },
+  {
     path: "/",
     component: Home,
     name: "home"
+  },
+  {
+    path: "/videos",
+    component: Videos,
+    name: "videos",
+    beforeEnter: (to, from, next) => {
+      if (!store.state.user) {
+        next("/");
+      } else {
+        next();
+      }
+    }
+  },
+  {
+    path: "/users",
+    component: Users,
+    name: "users",
+    beforeEnter: (to, from, next) => {
+      if (!(store.state.user && store.state.user.teacher)) {
+        next("/");
+      } else {
+        next();
+      }
+    }
   },
   {
     path: "/videos-management",
