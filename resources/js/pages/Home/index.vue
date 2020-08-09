2ada033cb09dd2092fbedd5d21acd192b1fc6141
Bruno Gustavo <brugucolosil95@gmail.com>
2020-08-09 01:21:18 -0300

    feat: add new page home

---
+++ resources/js/pages/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container h-75 d-flex  align-items-center">
+    <div class="mt-5">
+      <h1 class="display-3">Welcome to</h1>
+      <h1 class="display-3">Watch&Learn</h1>
+      <h1 class="display-3">platform!</h1>
+      <h1 class="text-muted mt-5">
+        Login or Register to have access to all videos!
+      </h1>
+      <h1 class="blockquote text-muted mt-5">" — A good start is half."</h1>
+    </div>
+    <!-- <div>
+      <img src="" class="img-fluid" alt="Responsive image">
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
